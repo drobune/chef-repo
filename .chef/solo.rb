fa0201846bcdc58a1e0ddc9d75ed29b9e2504bc6
yu1ch1 <s.yuichi227@gmail.com>
2014-02-02 07:15:51 +0900

    add berksfile user

---
+++ .chef/solo.rb
@@ -0,0 +1,2 @@
+file_cache_path "/tmp/chef-solo"
+cookbook_path "/home/yu1ch1/chef-repo/site-cookbooks"
--- site-cookbooks/jge-admin/recipes/default.rb
+++ site-cookbooks/jge-admin/recipes/default.rb
@@ -6,3 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package "vim" do
+  action :install
+end
+
+rcfile = File.join(node[:vim][:dir], ".vimrc")
+template rcfile do
+  source "vimrc.erb"
+end
+
+file rcfile do
+  owner node[:vim][:user]
+end
